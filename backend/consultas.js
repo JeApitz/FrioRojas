97c9589716db64534383ca8e650d5d71f2e083af
JeApitz <jeapitz360@gmail.com>
2025-03-10 18:31:28 -0300

    change

--- backend/consultas.js
+++ backend/consultas.js
@@ -2,10 +2,10 @@ import bcrypt from "bcryptjs";
 import pkg from "pg";
 const { Pool } = pkg;
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "123456",
-  database: "friorojas",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: 5432,
   allowExitOnIdle: true,
 });
